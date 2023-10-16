a868a6d98e641a5cb612f26a5f13a564aeb4f57d
masonictemple4 <mason.tucker96@gmail.com>
2023-10-16 15:04:57 -0400

    feat: env update.

--- next.config.js
+++ next.config.js
@@ -8,6 +8,18 @@ module.exports = withPlugins([
   }],
   {
     reactStrictMode: true,
+    env: {
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
+      BASE_API_URL: process.env.BASE_API_URL || 'http://localhost:8080',
+      PDF_RESUME_URL: process.env.PDF_RESUME_URL || 'https://storage.googleapis.com/theswequarry-pub/users/masonictemple4-1/resumes/Mason-Resume-2023.pdf',
+      MD_RESUME_URL: process.env.MD_RESUME_URL || 'https://storage.googleapis.com/theswequarry-pub/users/masonictemple4-1/resumes/RESUME.md',
+    },
+    publicRuntimeConfig: {
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
+      BASE_API_URL: process.env.BASE_API_URL || 'https://test.masonictemple4.app',
+      PDF_RESUME_URL: process.env.PDF_RESUME_URL || 'https://storage.googleapis.com/theswequarry-pub/users/masonictemple4-1/resumes/Mason-Resume-2023.pdf',
+      MD_RESUME_URL: process.env.MD_RESUME_URL || 'https://storage.googleapis.com/theswequarry-pub/users/masonictemple4-1/resumes/RESUME.md',
+    },
     images: {
       domains: [
         'cdn.sanity.io',
--- src/utils/globals.js
+++ src/utils/globals.js
@@ -1,3 +1,4 @@
-export const BASE_API_URL = 'http://localhost:8080';
-export const PDF_RESUME_URL = 'https://storage.googleapis.com/masonictemple4-pub/users/masonictemple4-1/resumes/Mason-Resume-2023.pdf'
-export const MD_RESUME_URL = 'https://storage.googleapis.com/masonictemple4-pub/users/masonictemple4-1/resumes/RESUME.md'
+// export const BASE_API_URL = 'http://localhost:8080';
+export const BASE_API_URL = 'https://masonictemple4.app';
+export const PDF_RESUME_URL = 'https://storage.googleapis.com/theswequarry-pub/users/masonictemple4-1/resumes/Mason-Resume-2023.pdf'
+export const MD_RESUME_URL = 'https://storage.googleapis.com/theswequarry-pub/users/masonictemple4-1/resumes/RESUME.md'
